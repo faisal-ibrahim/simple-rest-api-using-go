f67f7044a1d6783622c17c46e71ac2156531b213
Faisal Ibrahim <faisal.ibrahim@limango.com>
2022-03-15 20:54:27 +0100

    structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers to the database connections
+type App struct {
+}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting up our App!")
+	return nil
+}
 func main() {
 	fmt.Println("Simple REST API using GO!")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting our REST API")
+		fmt.Println(err)
+	}
 }
